614b4ed1fb24faa782e0465ef40d79d7331aef31
Udari Amarasinghe <Udari36@gmail.com>
2020-06-06 14:16:57 -0400

    Add files via upload

---
+++ perceptronw.py
@@ -0,0 +1,43 @@
+w1=(100), (101)
+w2= (110), (111)
+
+#formula is f(x) =x2
+fx= w1[0]+w1[1]
+fx1=w1[0]+w1[1]
+fx2=w2[0]+w2[1]
+fx3=w2[0]+w2[1]
+# if the formula for g(x) is x+2, and we want g(f(x)
+gx=fx+2
+gx1=fx1+2
+gx2=fx2+2
+gx3=fx3+2
+
+print (fx, fx1, fx2, fx3)
+print(gx, gx1, gx2, gx3)
+
+#4
+xd=1
+summation=[1/2, 1, 2]
+k1=1
+k2=2
+x1=2
+x2=2
+new=xd+summation[0]
+new1=x1+summation[1]
+new2=x2+summation[2]
+zm=1
+yk=2
+y1=3
+z0=3
+xdprocessed=(zm-yk)+new
+x1processed=(x1-yk*z0)+new1
+x2processed=(x1-yk*z0)+new2
+x0=(x1-yk*z0)+new2
+
+print(new)
+print(new1)
+print(new2)
+print(xdprocessed)
+print(x1processed)
+print(x2processed)
+print(x0)
